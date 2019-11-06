3fd55e75ce9298c8dae1247298e86dc827c41cff
Amanda <alewis3@stedwards.edu>
2019-11-06 02:44:47 -0600

    Changed port number for node app and added routing for react

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var path = require("path");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+if(process.env.NODE_ENV === 'production') {
+  router.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, '/../client/build', 'index.html'));
+  });
+}
+else {
+  router.get('/', (req, res) => {  
+    res.sendFile(path.join(__dirname+'/../client/public/index.html'));
+  });
+}
 
 module.exports = router;
